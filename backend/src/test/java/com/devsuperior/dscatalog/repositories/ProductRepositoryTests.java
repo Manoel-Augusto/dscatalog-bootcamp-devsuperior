4401cee641b4cc8ff1560fa2e7fcc8ba2f9459f0
Manoel <manoelll@gmail.com>
2021-05-20 16:45:12 -0300

    Refactor pageable

---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscatalog.repositories;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+}
