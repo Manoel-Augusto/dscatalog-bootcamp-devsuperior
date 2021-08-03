060cf1d62fd88b761c8816a21de7fe2b2e949f41
Manoel-Augusto <manoelll@gmail.com>
2021-08-03 18:49:18 -0300

    Check user authenticated

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,6 +1,16 @@
 import QueryString from 'qs';
 import axios, { AxiosRequestConfig } from 'axios';
 import history from './history';
+import { NumericLiteral } from '@babel/types';
+import jwtDecode from 'jwt-decode';
+
+type Role='ROLE_OPERATOR'| 'ROLE_ADMIN';
+
+type TokenData={
+    exp:number;
+    user_name:string;
+    authorities:Role[];
+}
 
 type LoginResponse = {
   access_token: string;
@@ -84,3 +94,17 @@ axios.interceptors.response.use(function (response) {
 
     return Promise.reject(error);
   });
+
+  export const getTokenData=():TokenData|undefined=>{
+      try{
+        return jwtDecode(getAuthData().access_token) as TokenData;
+      }
+      catch(error){
+          return undefined;
+      }
+    }
+
+      export const isAuthenticated = () : boolean=>{
+        const tokenData=getTokenData();
+        return (tokenData&&tokenData.exp*1000>Date.now())?true:false;
+    }
